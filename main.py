b8bb4ca7daaf64cf3a92372c2c230e340360d670
Gunbard <gunbard@gmail.com>
2022-01-01 17:00:00 -0600

    Adding initial state

---
+++ main.py
@@ -0,0 +1,77 @@
+import cv2, json, os, subprocess, asyncio, quamash, sys
+from enum import Enum
+from mainWindow import Ui_MainWindow
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QImage, QPixmap
+
+APP_TITLE = 'FindFrame'
+VERSION = '0.0.1'
+
+def open_image_path():
+    path = QtWidgets.QFileDialog.getOpenFileName(None, "Select Image", os.getcwd(), "Images (*.png *.jpg);;idgaf (*.*)")
+    if not path[0]:
+        print("No image selected!")
+        return
+    ui.fieldInputImage.setText(os.path.normpath(path[0]))
+    analyze_image(path[0])
+
+def open_video_path():
+    path = QtWidgets.QFileDialog.getOpenFileName(None, "Select Video", os.getcwd(), "Videos (*.mp4 *.mkv *.webm);;idgaf (*.*)")
+    if not path[0]:
+        print("No video selected!")
+        return
+    ui.fieldVideo.setText(os.path.normpath(path[0]))
+    ui.textLog.append("Opened video {}".format(os.path.basename(path[0])))
+
+    video = cv2.VideoCapture(path[0])
+    if not video.isOpened():
+        ui.textLog.append("Failed to open {}".format(os.path.basename(path[0])))
+        return
+
+    total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT) # 7 = prop-id
+    ui.textLog.append("Total frames: {}".format(total_frames))
+
+    video.set(cv2.CAP_PROP_POS_FRAMES, 10) # Seek to frame 10
+    success, frame = video.read()
+    rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    height, width, channel = rgbImage.shape
+    qImg = QImage(rgbImage.data, width, height, QImage.Format_RGB888)
+    ui.imageVideoFrame.setPixmap(QPixmap(qImg))
+    time = video.get(cv2.CAP_PROP_POS_MSEC)
+    ui.textLog.append("Time at frame 10: {}".format(time))
+
+def analyze_image(image):
+    parsed_image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    
+    height, width = parsed_image.shape
+    bytesPerLine = 1 * width
+    qImg = QImage(parsed_image.data, width, height, bytesPerLine, QImage.Format_Grayscale8)
+    ui.imageInput.setPixmap(QPixmap(qImg))
+    ui.textLog.append('Parsed input image.')
+    
+    orb = cv2.ORB_create()
+    keypoints, descriptors = orb.detectAndCompute(parsed_image, None)
+    brute_force_matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = brute_force_matcher.match(descriptors, descriptors)
+    return
+
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+app = QtWidgets.QApplication(sys.argv)
+loop = quamash.QEventLoop(app)
+asyncio.set_event_loop(loop)
+
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
+
+# EVENTS
+ui.btnOpenInputImage.clicked.connect(open_image_path)
+ui.btnOpenVideo.clicked.connect(open_video_path)
+
+MainWindow.setWindowTitle("{} {}".format(APP_TITLE, VERSION))
+MainWindow.show()
+
+with loop:
+    loop.run_forever()
---
+++ mainWindow.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\findFrameMain.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(512, 240)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(512, 240))
+        MainWindow.setMaximumSize(QtCore.QSize(512, 240))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 71, 21))
+        self.label.setObjectName("label")
+        self.fieldInputImage = QtWidgets.QLineEdit(self.centralwidget)
+        self.fieldInputImage.setGeometry(QtCore.QRect(90, 10, 361, 20))
+        self.fieldInputImage.setObjectName("fieldInputImage")
+        self.btnOpenInputImage = QtWidgets.QPushButton(self.centralwidget)
+        self.btnOpenInputImage.setGeometry(QtCore.QRect(460, 10, 41, 21))
+        self.btnOpenInputImage.setObjectName("btnOpenInputImage")
+        self.imageInput = QtWidgets.QLabel(self.centralwidget)
+        self.imageInput.setGeometry(QtCore.QRect(250, 110, 121, 91))
+        self.imageInput.setFrameShape(QtWidgets.QFrame.Panel)
+        self.imageInput.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.imageInput.setText("")
+        self.imageInput.setScaledContents(True)
+        self.imageInput.setObjectName("imageInput")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 40, 71, 21))
+        self.label_2.setObjectName("label_2")
+        self.fieldVideo = QtWidgets.QLineEdit(self.centralwidget)
+        self.fieldVideo.setGeometry(QtCore.QRect(90, 40, 361, 20))
+        self.fieldVideo.setObjectName("fieldVideo")
+        self.btnOpenVideo = QtWidgets.QPushButton(self.centralwidget)
+        self.btnOpenVideo.setGeometry(QtCore.QRect(460, 40, 41, 23))
+        self.btnOpenVideo.setObjectName("btnOpenVideo")
+        self.textLog = QtWidgets.QTextEdit(self.centralwidget)
+        self.textLog.setGeometry(QtCore.QRect(10, 110, 231, 91))
+        self.textLog.setFrameShape(QtWidgets.QFrame.Panel)
+        self.textLog.setUndoRedoEnabled(False)
+        self.textLog.setReadOnly(True)
+        self.textLog.setObjectName("textLog")
+        self.btnStartScan = QtWidgets.QPushButton(self.centralwidget)
+        self.btnStartScan.setGeometry(QtCore.QRect(180, 70, 161, 31))
+        self.btnStartScan.setObjectName("btnStartScan")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(10, 210, 491, 20))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setTextVisible(False)
+        self.progressBar.setInvertedAppearance(False)
+        self.progressBar.setObjectName("progressBar")
+        self.imageVideoFrame = QtWidgets.QLabel(self.centralwidget)
+        self.imageVideoFrame.setGeometry(QtCore.QRect(380, 110, 121, 91))
+        self.imageVideoFrame.setFrameShape(QtWidgets.QFrame.Panel)
+        self.imageVideoFrame.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.imageVideoFrame.setText("")
+        self.imageVideoFrame.setScaledContents(True)
+        self.imageVideoFrame.setObjectName("imageVideoFrame")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "FindFrame"))
+        self.label.setText(_translate("MainWindow", "Input image:"))
+        self.btnOpenInputImage.setText(_translate("MainWindow", "..."))
+        self.label_2.setText(_translate("MainWindow", "Video:"))
+        self.btnOpenVideo.setText(_translate("MainWindow", "..."))
+        self.btnStartScan.setText(_translate("MainWindow", "Scan Video"))
